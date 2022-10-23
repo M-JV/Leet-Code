22946f6dd57c6f2f7a7b9ce8a10d50da985caba3
Melvin John <melvinjohn2001@gmail.com>
2022-10-23 15:26:37 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+	    int max = candies[0];
+	    
+        for (int i = 0; i < candies.length; i++) {
+		// for all kids
+            max = Math.max(candies[i], max);
+		//setting maximum
+        }
+	    
+        List<Boolean> b = new ArrayList<>();
+	    // created a boolean list of name b
+        for (int i = 0; i < candies.length; i++) {
+		   b.add(candies[i] + extraCandies >= max);
+		// adding number of candies with ith kid and extra candies and comparing it with max, if it is greater than max treturn true.
+        }
+        return b;
+    }
+}
---
+++ Q8.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int max = 0,i;
+        //comparing value at nums[i] and max
+        for(i=0;i<nums.length;i++){
+            if(nums[i]>max)
+                // etting new max
+                max = nums[i];
+        }
+        
+        int B[] = new int[nums.length];
+        int C[] = new int[max+1];
+        
+        // initializing all elements of Count as 0
+        for(i=0;i<=max;i++) C[i] = 0;
+        
+        // Setting Count
+        for(i=0;i<nums.length;i++) C[nums[i]] = C[nums[i]] + 1;
+        
+        for(i=1;i<=max;i++) C[i] = C[i] + C[i-1];
+        //Checking if ccount is 0
+        for(i=0;i<nums.length;i++){
+            if(nums[i]==0) B[i]=0;
+            
+            // Setting the ans as count
+            else B[i] = C[nums[i]-1];
+        }
+        return B;
+    }
+}
